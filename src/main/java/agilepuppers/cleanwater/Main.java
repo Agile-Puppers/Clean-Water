cec67fdeda531ccbbad9ed3a67a385738e10a554
Gábor Siffel <hugabor@users.noreply.github.com>
2016-09-16 12:25:41 -0400

    Main class

---
+++ src/main/java/agilepuppers/cleanwater/Main.java
@@ -0,0 +1,18 @@
+package agilepuppers.cleanwater;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO
+        // helpful: http://yennicktrevels.com/blog/2013/10/15/javafx-structuring-your-application-overview/
+    }
+
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+}
