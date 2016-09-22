65a3742461f7fef6f458708d77ee3e207eb1e6c4
Gábor Siffel <hugabor@users.noreply.github.com>
2016-09-21 21:44:57 -0400

    login boilerplate and stuff
    and some more refactoring

--- src/main/java/agilepuppers/cleanwater/App.java
+++ src/main/java/agilepuppers/cleanwater/App.java
@@ -1,17 +1,12 @@
 package agilepuppers.cleanwater;
 
+import agilepuppers.cleanwater.controller.Controller;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class App extends Application {
 
     public static final String NAME = "Clean Water";
-
-    private Stage primaryStage;
 
     public static void main(String[] args) {
         // Launch the application
@@ -20,15 +15,18 @@ public class App extends Application {
     }
 
     private void load() {
-        // anything you want to load/do before starting the application, put here
-        SceneSwitcher.setPrimaryStage(primaryStage);
+        Controller.APP = this;
+
+        // anything you want to load/do before starting the application, put under here
+
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         load();
 
-        this.primaryStage = primaryStage;
+        // set up SceneSwitcher
+        SceneSwitcher.setPrimaryStage(primaryStage);
 
         primaryStage.setTitle(App.NAME);
 
---
+++ src/main/java/agilepuppers/cleanwater/controller/Controller.java
@@ -0,0 +1,10 @@
+package agilepuppers.cleanwater.controller;
+
+import agilepuppers.cleanwater.App;
+
+public abstract class Controller {
+    // make sure all controllers extend this class so they have access to the APP object
+
+    public static App APP;
+
+}
