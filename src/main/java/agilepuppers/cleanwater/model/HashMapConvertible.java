0334b17093a7f244182afc2dd2fbf509cd43433b
Cal Stephens <cal@calstephens.tech>
2016-09-28 10:19:37 -0400

    Back accounts database by a set of key-value pairs instead of an order-dependant array

---
+++ src/main/java/agilepuppers/cleanwater/model/HashMapConvertible.java
@@ -0,0 +1,35 @@
+package agilepuppers.cleanwater.model;
+
+import java.util.HashMap;
+
+/**
+ * Created by cal on 9/28/16.
+ */
+public abstract class HashMapConvertible {
+
+    //builds an instance of the object from a HashMap
+    public HashMapConvertible(HashMap<String, String> source) {
+        //override this
+    }
+
+    public HashMapConvertible() { }
+
+    public abstract HashMap<String, String> convertToHashMap();
+
+    //converts the result of convertToHashMap() to a comma-separated list of key-value pairs
+    public String serialize() {
+        StringBuilder builder = new StringBuilder();
+
+        HashMap<String, String> map = this.convertToHashMap();
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+
+            builder.append(key + "=" + value + ",");
+        }
+
+        //create a string and trim the trailing comma
+        String output = builder.toString().substring(0, builder.length() - 1);
+        return output;
+    }
+
+}
