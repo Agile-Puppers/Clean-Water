10e8bfb6fed1b98801f209a01f40f516712f5f9b
Joremih Wunter <jeremydwinter@gmail.com>
2016-10-03 14:52:53 -0400

    Added javadocs

--- src/main/java/agilepuppers/cleanwater/controller/HomeScreen.java
+++ src/main/java/agilepuppers/cleanwater/controller/HomeScreen.java
@@ -7,19 +7,27 @@ import javafx.scene.control.Label;
 public class HomeScreen {
 
     @FXML private Label usernameLabel;
-
+    
+    /**
+     * Initializes Home screen
+     */
     @FXML
     private void initialize() {
         String username = App.current.getUser().getUsername();
         this.usernameLabel.setText(username);
     }
 
+    /**
+     * Sets scene to Login screen and logs the user out
+     */
     @FXML
     private void handleLogout() {
         App.current.setUser(null);
         App.current.setScene("LoginScreen");
     }
-
+    /**
+     * Sets scene to the Edit Profile screen
+     */
     @FXML
     private void openEditProfile() {
         App.current.setScene("EditProfileScreen");
