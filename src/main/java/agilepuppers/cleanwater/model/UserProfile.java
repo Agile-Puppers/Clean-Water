034dc3f16b7aa30219bf03ef26ab86ab9bc9efbb
Nathan Lai <npylai@users.noreply.github.com>
2016-10-03 12:54:09 -0400

    Javadocs for UserProfile

--- src/main/java/agilepuppers/cleanwater/model/UserProfile.java
+++ src/main/java/agilepuppers/cleanwater/model/UserProfile.java
@@ -15,6 +15,9 @@ public class UserProfile {
     private String EMAIL;
     private String ADDRESS;
 
+    /**
+     * Base constructor for creating a new User Profile.
+     */
     public UserProfile() {
         this.TITLE = "";
         this.NAME = "";
@@ -22,6 +25,13 @@ public class UserProfile {
         this.ADDRESS = "";
     }
 
+    /**
+     * Constructor for a new User Profile
+     * @param title the users title
+     * @param name the users name
+     * @param email the users email address
+     * @param address the users home address
+     */
     public UserProfile(String title, String name, String email, String address) {
         this.TITLE = title;
         this.NAME = name;
@@ -29,19 +39,61 @@ public class UserProfile {
         this.ADDRESS = address;
     }
 
+    /**
+     * Setter for the users title
+     * @param title the users title
+     */
     public void setTitle(String title) { this.TITLE = title; }
+
+    /**
+     * Setter for the users name
+     * @param name the users name
+     */
     public void setName(String name) { this.NAME = name; }
+
+    /**
+     * Setter for the users email address
+     * @param email the users email address
+     */
     public void setEmail(String email) { this.EMAIL = email; }
+
+    /**
+     * Setter for the users home address
+     * @param address the users home address
+     */
     public void setAddress(String address) { this.ADDRESS = address; }
 
+    /**
+     * Getter for the users title
+     * @return the users title
+     */
     public String getTitle() { return TITLE; }
+
+    /**
+     * Getter for the users name
+     * @return the users name
+     */
     public String getName() { return NAME; }
+
+    /**
+     * Getter for the users email address
+     * @return the users email address
+     */
     public String getEmail() { return EMAIL; }
+
+    /**
+     * Getter for the users home address
+     * @return the users home address
+     */
     public String getAddress() { return ADDRESS; }
 
 
     //serialization methods
 
+    /**
+     * Converts the users title, name, email address, and home address into a hash map
+     * @return the hash map of the user profile instance variables
+     */
     public HashMap<String, String> convertToHashMap() {
         HashMap<String, String> map = new HashMap<>();
         map.put(TITLE_KEY, TITLE);
@@ -52,6 +104,10 @@ public class UserProfile {
         return map;
     }
 
+    /**
+     * Converts a user profile hash map into a new user profile
+     * @param hashMap the hash map of the user profile instance variables
+     */
     public UserProfile(HashMap<String, String> hashMap) {
         this.TITLE = hashMap.get(TITLE_KEY);
         this.NAME = hashMap.get(NAME_KEY);
