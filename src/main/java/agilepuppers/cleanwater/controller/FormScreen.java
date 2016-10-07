21243f42fa4c7bf9a54cc04cde75dd5dda71386a
Gábor Siffel <hugabor@users.noreply.github.com>
2016-10-07 00:47:10 -0400

    displayMessage() javadoc

--- src/main/java/agilepuppers/cleanwater/controller/FormScreen.java
+++ src/main/java/agilepuppers/cleanwater/controller/FormScreen.java
@@ -2,6 +2,11 @@ package agilepuppers.cleanwater.controller;
 
 public interface FormScreen {
 
+    /**
+     * Displays a form feedback message
+     *
+     * @param message message to be displayed
+     */
     public void displayMessage(String message);
 
 }
