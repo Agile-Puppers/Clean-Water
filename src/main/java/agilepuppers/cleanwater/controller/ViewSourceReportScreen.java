cc1aec2c1bb332af0b8f13a24dc35bbed4ff3e8d
Gábor Siffel <hugabor@users.noreply.github.com>
2016-10-07 01:03:16 -0400

    mistake

--- src/main/java/agilepuppers/cleanwater/controller/ViewSourceReportScreen.java
+++ src/main/java/agilepuppers/cleanwater/controller/ViewSourceReportScreen.java
@@ -1,10 +1,5 @@
 package agilepuppers.cleanwater.controller;
 
-public class ViewSourceReportScreen extends Controller implements FormScreen {
-
-    @Override
-    public void displayMessage(String message) {
-
-    }
+public class ViewSourceReportScreen extends Controller {
 
 }
